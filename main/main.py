3ef8b0fae42e1b1a10e375ef5fb9c0fe011980ba
shakik19 <1ne9ine.o@gmail.com>
2024-04-01 23:14:15 +0600

    added date to gcs filepath

--- main/main.py
+++ main/main.py
@@ -4,12 +4,14 @@ import schemas
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
+from datetime import datetime
 from pyarrow import fs
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 def download_dataset():
     dataset_name= "davidcariboo/player-scores"
     destination_dir= "../dataset/raw/"
+
     api = KaggleApi()
     api.authenticate()
     api.dataset_download_files(dataset_name, path=destination_dir, unzip=True)
@@ -18,7 +20,8 @@ def download_dataset():
 def define_schema(file_name: str) -> pd.DataFrame:
     path = f'../dataset/raw/{file_name.lower()}.csv'
     schema_class = getattr(schemas, file_name)
-    logging.info(f'📃 Setting schema for {file_name}')
+
+    logging.info(f'Setting schema for {file_name}')
     return pd.read_csv(path, dtype=schema_class.schema,
                        parse_dates=schema_class.date_cols)
 
@@ -41,23 +44,25 @@ def define_schema_and_upload_to_gcs():
 
     for file_name in csv_file_names:
         arrow_table = pa.Table.from_pandas(define_schema(file_name))
-        logging.info(f'👉 Set schema of {file_name} and parsed to Arrow table')
+        logging.info(f'Set schema of {file_name} and parsed to Arrow table')
 
-        gcs_path = f'{bucket_name}/final/{file_name.lower()}.parquet'
+        current_date = datetime.now().strftime('%m-%Y')
+        gcs_path = f'{bucket_name}/{current_date}/{file_name.lower()}.parquet'
+
         with target_fs.open_output_stream(gcs_path) as out:
-            logging.info(f'📂 Opened GCS output stream with path: {gcs_path}')
+            logging.info(f'Opened GCS output stream with path: {gcs_path}')
             try:
                 pq.write_table(arrow_table, out)
-                logging.info(f'✅ Wrote file {file_name.lower()}.parquet\n')
+                logging.info(f'Wrote file {file_name.lower()}.parquet\n')
             except Exception as e:
-                logging.error(f'❌ Couldn\'t write {gcs_path}')
+                logging.error(f'Couldn\'t write {gcs_path}')
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,
                         datefmt='%m-%d %H:%M',
                         format='%(asctime)s %(name)s - %(levelname)s - %(message)s',
                         handlers=[logging.FileHandler('app.log'), logging.StreamHandler()])
-    logging.info("🔰 START")
+    logging.info("START")
     define_schema_and_upload_to_gcs()
-    logging.info('Task completed 🔚\n\n')
+    logging.info('Task completed\n\n')
 
