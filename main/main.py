4d6a4501ab0cba6f2c8f18f538aa99684691a3d7
shakik19 <1ne9ine.o@gmail.com>
2024-04-02 02:03:10 +0600

    load data to bq completed

--- main/main.py
+++ main/main.py
@@ -7,6 +7,7 @@ import pyarrow.parquet as pq
 from datetime import datetime
 from pyarrow import fs
 from kaggle.api.kaggle_api_extended import KaggleApi
+from google.cloud import bigquery
 
 
 def download_dataset():
@@ -49,12 +50,35 @@ def define_schema_and_upload_to_gcs():
                 logging.error(f'Couldn\'t write to {gcs_path}')
 
 
+def load_data_gcs_to_bq():
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "../.creds/gcp_key.json"
+    client = bigquery.Client()
+    date = datetime.now().strftime('%m-%Y')
+    
+    for name in schemas.class_names:
+        name = name.lower()
+        query = f"""
+            LOAD DATA OVERWRITE final_project.{name}
+            FROM FILES (
+            format = 'PARQUET',
+            uris = ['gs://transfermarkt-project/{date}/{name}.parquet']);
+        """
+
+        logging.info(f'Loading {name} table')
+        try:
+            client.query(query)
+            logging.info(f'Successfully loaded {name} table\n')
+        except TypeError as e:
+            logging.error(e)
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,
                         datefmt='%m-%d %H:%M',
                         format='%(asctime)s %(name)s - %(levelname)s - %(message)s',
                         handlers=[logging.FileHandler('app.log'), logging.StreamHandler()])
     logging.info("START")
-    define_schema_and_upload_to_gcs()
+    # define_schema_and_upload_to_gcs()
+    # load_data_gcs_to_bq()
     logging.info('Task completed\n\n')
 
