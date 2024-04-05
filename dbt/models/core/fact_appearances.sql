d22288ac9aae8b04a0c0a7f7b6280fb997ae70ec
1ne9ine.o@gmail.com <1ne9ine.o@gmail.com>
2024-04-05 17:53:49 +0000

    extra cte added for date anomalies

--- dbt/models/core/fact_appearances.sql
+++ dbt/models/core/fact_appearances.sql
@@ -10,22 +10,28 @@
     )
 }}
 
+WITH cte_appearances AS(
+    SELECT
+        appearance_id,
+        game_id,
+        player_id,
+        player_club_id,
+        player_current_club_id,
+        competition_id,
+        date AS match_date,
+        player_name,
+        yellow_cards,
+        red_cards,
+        goals,
+        assists,
+        minutes_played
+    FROM
+        {{ source("raw", "appearances") }}
+)
 SELECT
-    appearance_id,
-    game_id,
-    player_id,
-    player_club_id,
-    player_current_club_id,
-    competition_id,
-    date AS match_date,
-    player_name,
-    yellow_cards,
-    red_cards,
-    goals,
-    assists,
-    minutes_played
+    *
 FROM
-    {{ source("raw", "appearances") }}
+    cte_appearances
 
 {% if is_incremental() %}
 WHERE match_date > (SELECT MAX(match_date) FROM {{ this }})
--- dbt/models/core/fact_game_lineups.sql
+++ dbt/models/core/fact_game_lineups.sql
@@ -1,16 +1,24 @@
+WITH cte_game_lineups AS(
+    SELECT
+        game_lineups_id,
+        game_id,
+        date AS match_date,
+        player_id,
+        club_id,
+        player_name,
+        type,
+        position AS playing_position,
+        number,
+        team_captain
+    FROM
+        {{ source("raw", "game_lineups") }}
+)
+
 SELECT
-    game_lineups_id,
-    game_id,
-    date AS match_date,
-    player_id,
-    club_id,
-    player_name,
-    type,
-    position AS playing_position,
-    number,
-    team_captain
+    *
 FROM
-    {{ source("raw", "game_lineups") }}
+    cte_game_lineups
+
 {% if is_incremental() %}
 WHERE match_date > (SELECT MAX(match_date) FROM {{ this }})
 {% endif %}
