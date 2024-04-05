7b50c887af99cd423bf3071250c3a2134fac66cf
1ne9ine.o@gmail.com <1ne9ine.o@gmail.com>
2024-04-05 14:23:20 +0000

    complete and created

---
+++ dbt/models/core/fact_games.sql
@@ -0,0 +1,29 @@
+SELECT
+    game_id,
+    competition_id,
+    season,
+    round AS match_day,
+    date as game_date,
+    home_club_id,
+    away_club_id,
+    home_club_goals,
+    away_club_goals,
+    home_club_position,
+    away_club_position,
+    home_club_manager_name,
+    away_club_manager_name,
+    stadium,
+    attendance,
+    referee,
+    home_club_formation,
+    away_club_formation,
+    home_club_name,
+    away_club_name,
+    aggregate,
+    competition_type
+FROM
+    {{ source("raw", "games") }}
+
+{% if is_incremental() %}
+WHERE game_date > (SELECT MAX(game_date) FROM {{ this }})
+{% endif %}
