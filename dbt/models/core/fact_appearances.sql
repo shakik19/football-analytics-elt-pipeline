ceabf707f1f83d61fe0501f8478adffefca16353
1ne9ine.o@gmail.com <1ne9ine.o@gmail.com>
2024-04-05 13:46:23 +0000

    complete

---
+++ dbt/models/core/fact_appearances.sql
@@ -0,0 +1,32 @@
+{{
+    config(
+        materialized = "incremental",
+        cluster_by = ["player_id", "player_club_id"],
+        partition_by = {
+        "field": "match_date",
+        "data_type": "date",
+        "granularity": "month"
+        }
+    )
+}}
+
+SELECT
+    appearance_id,
+    game_id,
+    player_id,
+    player_club_id,
+    player_current_club_id,
+    competition_id,
+    date AS match_date,
+    player_name,
+    yellow_cards,
+    red_cards,
+    goals,
+    assists,
+    minutes_played
+FROM
+    {{ source("raw", "appearances") }}
+
+{% if is_incremental() %}
+WHERE match_date > (SELECT MAX(match_date) FROM {{ this }})
+{% endif %}
