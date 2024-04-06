bcb7aadd096c27e85a6b701aac8417b77506b30a
shakik19 <1ne9ine.o@gmail.com>
2024-04-06 16:16:26 +0600

    add new setup and config

---
+++ dags/pipeline/variables.py
@@ -0,0 +1,18 @@
+PROJECT_NAME = ""
+BUCKET_NAME = "transfermarkt-data-storage"
+DATASET_NAME = "transfermarkt_seed"
+
+SERVICE_ACC_PATH = "./creds/gcp_key.json"
+
+RAW_DATASET_DIR = "./dataset/raw/"
+
+tables = [
+        'appearances',
+        'club_games',
+        'clubs',
+        'competitions',
+        'game_events',
+        'game_lineups',
+        'games',
+        'player_valuations',
+        'players']
