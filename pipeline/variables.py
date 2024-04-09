f7dad6a7226859faac85c6267c9903953a61dca1
shakik19 <1ne9ine.o@gmail.com>
2024-04-09 23:54:24 +0600

    update dbt vars

--- pipeline/variables.py
+++ pipeline/variables.py
@@ -1,11 +1,13 @@
 PROJECT_ID = "fast-forward-412713"
 BUCKET_NAME = "transfermarkt-data"
-DATASET_NAME = "transfermarkt_seed"
+SEED_DATASET_NAME = "transfermarkt_seed"
+CORE_DATASET_NAME = "transfermarkt_core"
 
-SERVICE_ACC_PATH = "./creds/gcp_key.json"
+SERVICE_ACC_PATH = "../credentials/gcp_key.json"
 
 RAW_DATASET_DIR = "./dataset/raw"
 PARQUET_DIR = "./dataset/parquet"
+DBT_PROJECT_DIR = "../dbt"
 
 tables = [
         'Appearances',
