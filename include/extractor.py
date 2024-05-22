32d94610040cd743a69b21b5efebac71b9b6a340
shakik19 <1ne9ine.o@gmail.com>
2024-05-22 11:31:55 +0600

    remove redundancies

--- include/extractor.py
+++ include/extractor.py
@@ -1,18 +1,14 @@
 import os
 import logging
-from dotenv import load_dotenv
-from transformer import DataTransformer
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 
 class DatasetDownloader:
-    def __init__(self, dataset_dir="../dataset"):
-        self.dataset_dir = dataset_dir
-        load_dotenv()
+    def __init__(self):
+        self.dataset_dir = os.environ.get("DATASET_DIR")
         self.logger = logging.getLogger(__name__)
 
     def download_dataset(self):
-        os.environ["KAGGLE_CONFIG_DIR"] = os.getenv("KAGGLE_API_KEY")
         dataset_name = "davidcariboo/player-scores"
         destination_dir = f"{self.dataset_dir}/csv"
 
@@ -22,5 +18,3 @@ class DatasetDownloader:
         api.dataset_download_files(dataset_name, path=destination_dir, unzip=True)
         self.logger.info("Download completed")
 
-        transformer = DataTransformer()
-        transformer.exclude_corrupt_columns()
