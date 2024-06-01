1d4781fc1eabade192d2421c3d368857b0949586
shakik19 <1ne9ine.o@gmail.com>
2024-06-01 10:50:07 +0600

    modify: parallelize loading tasks using taskgroups

--- dags/transfermarkt.py
+++ dags/transfermarkt.py
@@ -1,12 +1,14 @@
 import os
 from datetime import datetime
 from airflow import DAG
+from airflow.utils.task_group import TaskGroup
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 from include.extractor import DatasetDownloader
 from include.transformer import DataTransformer
-from include.loader import DataLoader
 from cosmos import (DbtTaskGroup,
                     ProjectConfig,
                     ProfileConfig,
@@ -20,10 +22,14 @@ with DAG(
     description="Transfermarkt ELT data pipeline",
     start_date=datetime(2024, 1, 1),
     schedule_interval=None,
-    tags=["transfermarkt", "ELT", "gcp", "bigquery", "cloud_storage","dbt"],
-    catchup=False
+    tags=["ELT", "gcp", "bigquery", "cloud_storage","dbt"],
+    catchup=False,
+    max_active_runs=1,
+    max_active_tasks=5
 ):
     PROJECT_NAME = "transfermarkt"
+    GCP_CONN_ID = os.getenv("GCP_CONN_ID")
+    DATASET_DIR = os.getenv("DATASET_DIR")
     start = EmptyOperator(task_id="start")
 
     """
@@ -66,36 +72,69 @@ with DAG(
     [Load]
     Uploading parquet files to Data Lake(Google Cloud Stroage) 
     """
-    t_loader = DataLoader(project_name=PROJECT_NAME)
-    load_datalake = PythonOperator(
-        task_id="load_datalake",
-        python_callable=t_loader.upload_to_gcs,
-    )
+    TABLES = t_transformer.TABLES
+    with TaskGroup(group_id="upload_files_to_gcs", 
+                   tooltip="Upload files to GCS") as upload_files_to_gcs:
+        for table in TABLES:
+            filename = f"{table}.parquet"
+            src_file_path = os.path.join(t_transformer.PARQUET_DATASET_DIR, filename)
+            dst_file_path = os.path.join(PROJECT_NAME, filename)
+            task_id = f"upload_{table}"
+            LocalFilesystemToGCSOperator(
+                task_id=task_id,
+                src=src_file_path,
+                dst=dst_file_path,
+                bucket=os.getenv("BUCKET_NAME"),
+                gcp_conn_id=GCP_CONN_ID
+            )
 
     """
     Loading data from Data Lake to Data Warehouse as native tables for 
     transformation and analysis
     """
-    load_seed_dataset = PythonOperator(
-        task_id="load_seed_dataset",
-        python_callable=t_loader.load_bigquery_seed_dataset,
-    )
+    with TaskGroup(group_id="load_files_into_bigquery", 
+                   tooltip="Load files into BigQuery") as load_files_into_bigquery:
+        for table in TABLES:
+            filename = f"{table}.parquet"
+            filepath = os.path.join(PROJECT_NAME, filename)
+            table_name = f"{os.getenv('PROJECT_ID')}.{os.getenv('SEED_DATASET_NAME')}.{table}"
+            gcs_filepath = os.path.join(os.getenv("BUCKET_NAME"), filepath)
+            query = f"""
+                LOAD DATA OVERWRITE `{table_name}`
+                FROM FILES (
+                format = 'PARQUET',
+                uris = ['gs://{gcs_filepath}']
+                );
+            """
+
+            load_table = BigQueryInsertJobOperator(
+                task_id=f'load_{table}_into_bigquery',
+                gcp_conn_id=GCP_CONN_ID,
+                configuration={
+                    "query": {
+                        "query": query,
+                        "useLegacySql": False,
+                    }
+                },
+                location=os.getenv("DATASET_LOCATION"),
+                result_timeout=30
+            )
 
 
     """
     [Transform]
     DBT transformation tasks
     """
-    DBT_PROJECT_DIR = os.environ.get("DBT_PROJECT_DIR")
-    DBT_EXE_PATH = os.environ.get("DBT_EXE_PATH")
+    DBT_PROJECT_DIR = os.getenv("DBT_PROJECT_DIR")
+    DBT_EXE_PATH = os.getenv("DBT_EXE_PATH")
 
     PROJECT_CONFIG = ProjectConfig(
         dbt_project_path=DBT_PROJECT_DIR
     )
 
     PROFILE_CONFIG = ProfileConfig(
-        profile_name=os.environ.get("DBT_PROFILES_NAME"),
-        target_name=os.environ.get("DBT_PROFILES_TARGET"),
+        profile_name=os.getenv("DBT_PROFILES_NAME"),
+        target_name=os.getenv("DBT_PROFILES_TARGET"),
         profiles_yml_filepath=f"{DBT_PROJECT_DIR}/profiles.yml"
     )
 
@@ -121,8 +160,7 @@ with DAG(
     END
     Before ending clering the local csv and parquet files
     """
-    project_dataset_path = f"{os.environ.get("DATASET_DIR")}/{PROJECT_NAME}"
-    bash_remove_command = f"rm -rf {project_dataset_path}"
+    bash_remove_command = f"rm -rfv {t_transformer.PROJECT_DATASET_DIR}"
     clean_local_dataset = BashOperator(
         task_id = "clean_local_dataset",
         bash_command=bash_remove_command
@@ -132,5 +170,5 @@ with DAG(
 
 
     start >> extract_raw_data >> pre_process_data >> convert_to_parquet
-    convert_to_parquet >> load_datalake >> load_seed_dataset
-    load_seed_dataset >> dbt_run_models >> clean_local_dataset >> end
+    convert_to_parquet >> upload_files_to_gcs >> load_files_into_bigquery
+    load_files_into_bigquery >> dbt_run_models >> clean_local_dataset >> end
