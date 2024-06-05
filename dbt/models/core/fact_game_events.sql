61d23c7b879f2fe88d893ef6a92fc371f87e343c
shakik19 <1ne9ine.o@gmail.com>
2024-06-05 15:01:08 +0600

    add: sub and clock filter

--- dbt/models/core/fact_game_events.sql
+++ dbt/models/core/fact_game_events.sql
@@ -3,8 +3,11 @@ WITH cte_game_events AS(
         game_event_id,
         {{ dbt.safe_cast("date", api.Column.translate_type("date")) }} AS ingestion_date,
         game_id,
-        src.minute AS clock,
-        src.type,
+        CASE 
+            WHEN src.minute < 0 THEN 1 
+            ELSE src.minute
+        END as clock,
+        src.type AS event_type,
         club_id,
         player_id,
         description,
@@ -12,6 +15,8 @@ WITH cte_game_events AS(
         player_assist_id
     FROM
         {{ source("raw", "game_events") }} src
+    WHERE
+        type = "Substitutions" AND player_in_id IS NOT NULL
 )
 
 SELECT
