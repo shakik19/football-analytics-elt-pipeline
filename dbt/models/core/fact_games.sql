c4cca26ea4e832ab17a522f1beab0f529348cfac
shakik19 <1ne9ine.o@gmail.com>
2024-06-05 19:17:25 +0600

    add: season details

--- dbt/models/core/fact_games.sql
+++ dbt/models/core/fact_games.sql
@@ -5,6 +5,15 @@ WITH cte_games AS(
         season,
         src.round AS match_day,
         {{ dbt.safe_cast("date", api.Column.translate_type("date")) }} as game_date,
+        EXTRACT(MONTH FROM date) AS game_month,
+        CASE 
+            WHEN EXTRACT(MONTH FROM date) BETWEEN 6 AND 12 THEN CONCAT(season, "/", season + 1)
+            ELSE CONCAT(season - 1, "/", season)
+        END AS full_season,
+        CASE 
+            WHEN EXTRACT(MONTH FROM date) BETWEEN 6 AND 12 THEN 'First Half'
+            ELSE 'Second Half'
+        END AS season_half,
         home_club_id,
         away_club_id,
         home_club_goals,
