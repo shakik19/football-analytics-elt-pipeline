68f7f85221b84ee216b42d928cb7edfc390ee0dd
shakik19 <1ne9ine.o@gmail.com>
2024-06-06 20:32:41 +0600

    remove: fact_games data agg to fact_club_games

--- dbt/models/core/fact_club_games.sql
+++ dbt/models/core/fact_club_games.sql
@@ -1,14 +1,68 @@
+WITH 
+cte_home AS(
+    SELECT
+        fcg.game_id,
+        fg.date AS game_date,
+        fcg.club_id AS own_id,
+        fcg.opponent_id,
+        fcg.own_goals,	
+        fcg.opponent_goals,	
+        fcg.own_position,
+        fcg.opponent_position,	
+        fcg.own_manager_name,	
+        fcg.opponent_manager_name,	
+        fg.home_club_formation AS home_club_formation,	
+        fg.away_club_formation AS opponent_club_formation,	
+        {{ is_win('fcg.own_goals', 'fcg.opponent_goals') }} AS is_win,
+        {{ get_points('fcg.own_goals', 'fcg.opponent_goals') }} AS points,
+        "home" AS game_location
+    FROM
+        {{ source("raw", "club_games") }} fcg
+    LEFT JOIN
+        {{ source("raw", "games") }} fg
+    ON
+        fcg.game_id = fg.game_id
+    WHERE
+        fcg.club_id = fg.home_club_id
+    {% if is_incremental() %}
+    AND fg.date > (SELECT MAX(date) FROM {{ source("raw", "games") }})
+    {% endif %}
+),
+cte_away AS(
+    SELECT
+        fcg.game_id,
+        fg.date AS game_date,
+        fcg.club_id AS own_id,
+        fcg.opponent_id,
+        fcg.own_goals,	
+        fcg.opponent_goals,	
+        fcg.own_position,
+        fcg.opponent_position,	
+        fcg.own_manager_name,	
+        fcg.opponent_manager_name,
+        fg.away_club_formation AS own_formation,	
+        fg.home_club_formation AS opponent_formation,	
+        {{ is_win('fcg.own_goals', 'fcg.opponent_goals') }} AS is_win,
+        {{ get_points('fcg.own_goals', 'fcg.opponent_goals') }} AS points,
+        "away" AS game_location	
+    FROM
+        {{ source("raw", "club_games") }} fcg
+    LEFT JOIN
+        {{ source("raw", "games") }} fg
+    ON
+        fcg.game_id = fg.game_id
+    WHERE
+        fcg.club_id = fg.away_club_id
+    {% if is_incremental() %}
+    AND fg.date > (SELECT MAX(date) FROM {{ source("raw", "games") }})
+    {% endif %}
+)
 SELECT
-    game_id,
-    club_id,
-    own_goals,
-    own_position,
-    own_manager_name,
-    opponent_id,
-    opponent_goals,
-    opponent_position,
-    opponent_manager_name,
-    hosting,
-    is_win
+    *
 FROM
-    {{ source("raw", "club_games") }}
+    cte_home
+UNION ALL
+SELECT
+    *
+FROM
+    cte_away
