4f90299c1b2fbae5417d65f771d3b235c4beb802
shakik19 <1ne9ine.o@gmail.com>
2024-06-14 20:57:52 +0600

    add: filter for current_club info

--- dbt/models/core/dim_players.sql
+++ dbt/models/core/dim_players.sql
@@ -56,7 +56,11 @@ players AS (
         last_name,
         name,
         last_season,
-        current_club_id,
+        CASE 
+            WHEN last_season = (SELECT MAX(last_season) FROM {{ source("raw", "players") }}) 
+                THEN current_club_id  
+            ELSE NULL
+        END AS current_club_id,
         {{ dbt.safe_cast("date_of_birth", api.Column.translate_type("date")) }} AS date_of_birth,
         country_of_birth,
         city_of_birth,
@@ -67,8 +71,6 @@ players AS (
         height_in_cm,
         agent_name,
         {{ dbt.safe_cast("contract_expiration_date", api.Column.translate_type("date")) }} as contract_expiration_date,
-        current_club_domestic_competition_id,
-        current_club_name
     FROM 
         {{ source("raw", "players") }}
 )
@@ -81,6 +83,7 @@ SELECT
     players.name,
     players.last_season,
     players.current_club_id,
+    clubs.name AS current_club_name,
     DATE_DIFF(CURRENT_DATE(), players.date_of_birth, YEAR) AS age,
     players.date_of_birth,
     players.country_of_birth,
@@ -92,8 +95,6 @@ SELECT
     players.height_in_cm,
     players.agent_name,
     players.contract_expiration_date,
-    players.current_club_domestic_competition_id,
-    players.current_club_name,
     cv.market_value_in_eur AS current_market_value_in_eur,
     mv.market_value_in_eur AS highest_market_value_in_eur
 FROM
@@ -104,3 +105,6 @@ FROM
     INNER JOIN
     max_values AS mv
     ON players.player_id = mv.player_id
+    LEFT JOIN
+    {{ source("raw", "clubs") }} clubs
+    ON clubs.club_id = players.current_club_id
