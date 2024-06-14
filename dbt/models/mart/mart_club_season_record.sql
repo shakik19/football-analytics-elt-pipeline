3dea15db47dd7d65840544d7f19240aebfa995f7
shakik19 <1ne9ine.o@gmail.com>
2024-06-15 00:46:57 +0600

    add: result fields

--- dbt/models/mart/mart_club_season_record.sql
+++ dbt/models/mart/mart_club_season_record.sql
@@ -7,11 +7,15 @@ SELECT
   SUM(fcg.own_goals) AS goals_scored,
   SUM(fcg.opponent_goals) AS goals_conceded,
   SUM(CASE WHEN fcg.opponent_goals = 0 THEN 1 ELSE 0 END) AS clean_sheets,
+  SUM(CASE WHEN fcg.is_win = 1 THEN 1 ELSE 0 END) AS wins,
+  SUM(CASE WHEN fcg.is_win = 0 THEN 1 ELSE 0 END) AS draws,
+  SUM(CASE WHEN fcg.is_win = -1 THEN 1 ELSE 0 END) AS losses,
   SUM(CASE WHEN dgi.round != "final" AND fcg.game_location = "home" AND fcg.is_win = 1 THEN 1 ELSE 0 END) AS home_wins,
   SUM(CASE WHEN dgi.round != "final" AND fcg.game_location = "home" AND fcg.is_win = -1 THEN 1 ELSE 0 END) AS home_losses,
   SUM(CASE WHEN dgi.round != "final" AND fcg.game_location = "away" AND fcg.is_win = 1 THEN 1 ELSE 0 END) AS away_wins,
   SUM(CASE WHEN dgi.round != "final" AND fcg.game_location = "away" AND fcg.is_win = -1 THEN 1 ELSE 0 END) AS away_losses,
-  AVG(CASE WHEN fcg.game_location = "home" THEN dgi.attendance END) AS avg_home_attendance
+  AVG(CASE WHEN fcg.game_location = "home" THEN dgi.attendance END) AS avg_home_attendance,
+  AVG(CASE WHEN fcg.game_location = "away" THEN dgi.attendance END) AS avg_away_attendance
 FROM
   `transfermarkt_core.fact_club_games` fcg
   LEFT JOIN `transfermarkt_core.dim_game_info` dgi USING(game_id)
